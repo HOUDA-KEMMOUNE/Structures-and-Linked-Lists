8b15d5e8f5b02d43b90f84e5320c4e36d07b5362
Houda Kemmoune <hkemmoun@c4r6p5.1337.ma>
2024-11-09 14:07:30 +0100

    update

--- single_lkl.c
+++ single_lkl.c
@@ -6,7 +6,44 @@ typedef struct		s_node
 	int				data;
 	struct s_node 	*next;
 }					t_node;
-/*
+
+void	count_node(t_node *head)
+{
+	t_node *ptr;
+	int count;
+
+	count = 0;
+	ptr = head;
+	if (head == NULL)
+		printf("Empty linked list :/\n");
+	while (ptr != NULL )
+	{
+		count++;
+		ptr = ptr->next;
+	}
+	if (count == 0 || count == 1)
+		printf("We have %d node\n", count);
+	else
+		printf("We have %d nodes\n", count);
+}
+
+void	print_node(t_node *head)
+{
+	t_node *ptr;
+	int i;
+	
+	i = 0;
+	ptr = head;
+	if (head == NULL)
+		printf("Empty linked list :/\n");
+	while (ptr != NULL)
+	{	
+		i++;
+		printf("Data of node %d:\t%d\n", i, ptr->data);
+		ptr = ptr->next;
+	}
+}
+
 int main()
 {
 	t_node *head = NULL;
@@ -22,11 +59,15 @@ int main()
 	tail->next = NULL;
 	head->next = tail;
 
-	tail->data = 76;
+	tail = NULL;
+	tail = (t_node *)malloc(sizeof(t_node));
+
+	tail->data = 75;
 	tail->next = NULL;
 	head->next->next = tail;
-
-	printf("The data of the first node:\t%d\n", head->data);
-	printf("The data of the last node:\t%d\n", tail->data);
+	//count_node(head);	
+	tail = NULL;
+	free(tail);
+	count_node(head);
+	print_node(head);
 }
-*/
