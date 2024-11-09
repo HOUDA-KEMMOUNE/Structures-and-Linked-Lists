7349baadcdec427d7c8ea9fc88a3f24ef0bfe87d
Houda Kemmoune <hkemmoun@c4r6p7.1337.ma>
2024-11-09 01:05:56 +0100

    update

---
+++ single_lkl.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct		s_node
+{
+	int				data;
+	struct s_node 	*next;
+}					t_node;
+/*
+int main()
+{
+	t_node *head = NULL;
+	t_node *tail = NULL;
+
+	head = (t_node *)malloc(sizeof(t_node));
+	tail = (t_node *)malloc(sizeof(t_node));
+
+	head->data = 44;
+	head->next = NULL;
+
+	tail->data = 52;
+	tail->next = NULL;
+
+	head->next = tail;
+
+	printf("The data of the first node:\t%d\n", head->data);
+	printf("The data of the second node:\t%d\n", tail->data);	
+
+	free(tail);
+}
+*/
